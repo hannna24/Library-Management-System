961deb62e225761ba9da1eea8eab0468c518910a
hanna <hanabakeer24@gmail.com>
2023-06-30 00:37:37 +0300

    Add Librarian.java

---
+++ Librarian.java
@@ -0,0 +1,9 @@
+package com.example.majortask;
+
+
+public class Librarian extends person {
+    public Librarian(String id, int password, String type, String firstName, String lastName, String address, int cellPhone, String email, boolean isBlocked) {
+        super(id, password, type, firstName, lastName, address, cellPhone, email, isBlocked);
+
+    }
+}
