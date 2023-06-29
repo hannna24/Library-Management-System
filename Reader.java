eeeab56640f051c8c352bca764dbd79e239875b5
hanna <hanabakeer24@gmail.com>
2023-06-30 00:38:42 +0300

    Add Reader.java

---
+++ Reader.java
@@ -0,0 +1,8 @@
+package com.example.majortask;
+
+
+public class Reader extends person {
+    public Reader(String id, int password, String type, String firstName, String lastName, String address, int cellPhone, String email, boolean isBlocked) {
+        super(id, password, type, firstName, lastName, address, cellPhone, email, isBlocked);
+    }
+}
